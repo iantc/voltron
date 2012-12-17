70b68fdef2e193905683d57a7e014e3256a4671c
Ian Chan <ichan@csusm.edu>
2012-12-17 10:35:39 -0800

    Improve front page UX

--- template.php
+++ template.php
@@ -4,3 +4,16 @@ function voltron_preprocess_html(&$variables) {
   drupal_add_css('http://netdna.bootstrapcdn.com/bootswatch/2.1.0/spacelab/bootstrap.no-icons.min.css', array('type' => 'external','group' => CSS_THEME,'weight' => 100));
   drupal_add_css('http://netdna.bootstrapcdn.com/font-awesome/2.0/css/font-awesome.css', array('type' => 'external'));
 }
+
+/**
+ * Custom theme function for the login/register link.
+ */
+function voltron_lt_login_link($variables) {
+  // Only display register text if registration is allowed.
+  if (variable_get('user_register', USER_REGISTER_VISITORS_ADMINISTRATIVE_APPROVAL)) {
+    return t('Sign In');
+  }
+  else {
+    return t('Log in');
+  }
+}
---
+++ theme-settings.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Implements hook_form_system_theme_settings_alter() function.
+ *
+ * @param $form
+ *   Nested array of form elements that comprise the form.
+ * @param $form_state
+ *   A keyed array containing the current state of the form.
+ */
+function voltron_form_system_theme_settings_alter(&$form, $form_state, $form_id = NULL) {
+  // Work-around for a core bug affecting admin themes. See issue #943212.
+  if (isset($form_id)) {
+    return;
+  }
+
+  // Create the form using Forms API
+  $form['themedev']['voltron_rebuild_registry'] = array(
+    '#type'          => 'checkbox',
+    '#title'         => t('Rebuild theme registry on every page.'),
+    '#default_value' => theme_get_setting('voltron_rebuild_registry'),
+    '#description'   => t('During theme development, it can be very useful to continuously <a href="!link">rebuild the theme registry</a>. WARNING: this is a huge performance penalty and must be turned off on production websites.', array('!link' => 'http://drupal.org/node/173880#theme-registry')),
+  );
+}
