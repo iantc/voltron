d11e566d63bdb52eb2028c7a8ab80768731f0929
Ian Chan <ichan@csusm.edu>
2013-01-25 22:52:59 -0800

    Apply standard table display to all search results

--- template.php
+++ template.php
@@ -17,3 +17,32 @@ function voltron_lt_login_link($variables) {
     return t('Log in');
   }
 }
+
+/**
+ * Implementation of preprocess_page().
+ */
+function voltron_preprocess_page(&$vars) {
+  $attr = array();
+
+  // Replace screen/all stylesheets with print
+  // We want a minimal print representation here for full control.
+  if (isset($_GET['print'])) {
+    $css = drupal_add_css();
+    unset($css['all']);
+    unset($css['screen']);
+    $css['all'] = $css['print'];
+    $vars['styles'] = drupal_get_css($css);
+
+    // Add print header
+    $vars['print_header'] = theme('print_header');
+
+    // Replace all body classes
+    $attr['class'] = 'print';
+
+    // Use print template
+    $vars['template_file'] = 'templates/print-page';
+
+    // Suppress devel output
+    $GLOBALS['devel_shutdown'] = FALSE;
+  }
+}
