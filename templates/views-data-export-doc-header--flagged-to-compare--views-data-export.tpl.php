d72e7ee56dbbf8ccf5dabe2f558e57e3a19901bc
Ian C <ian.t.c1995@gmail.com>
2013-06-22 21:33:43 -0700

    Custom Wod template for Views Data Export

---
+++ templates/views-data-export-doc-header--flagged-to-compare--views-data-export.tpl.php
@@ -0,0 +1,42 @@
+<html xmlns:o='urn:schemas-microsoft-com:office:office' xmlns:w='urn:schemas-microsoft-com:office:word' xmlns='http://www.w3.org/TR/REC-html40'>
+<head><title>My Features List</title>
+<style> <!--
+@page
+{
+    size: 8in 11in;  /* A4 */
+    margin: 1in 1in 1in 1in; /* Margins: 2 cm on each side */
+    mso-page-orientation: portrait;
+}
+-->
+body{font-family: Arial, Verdana, Helvetica, sans-serif;}
+table {
+max-width: 100%;
+background-color: rgba(0, 0, 0, 0);
+border-spacing: 0;
+width: 100%;border: 1px solid #DDD;
+border-collapse: collapse;
+border-left: 0;
+-webkit-border-radius: 4px;
+-moz-border-radius: 4px;
+border-radius: 4px;
+}
+th, td {
+padding: 8px;
+line-height: 20px;
+text-align: left;
+vertical-align: top;
+border-top: 1px solid #DDD;
+}
+th {
+font-weight: bold;
+border-left: 1px solid #DDD;
+}
+td {
+border-left: 1px solid #DDD;
+}
+</style>
+  </head>
+  <body>
+    <table>
+    <?php print $header_row; ?>
+    <tbody>
